199488701affc7751b6a3c1120b48aec4d1c853c
Angel-HW <43689768+Angel-HW@users.noreply.github.com>
2019-08-04 12:51:35 +0800

    Create web_crawler_doubanTOP250.py

---
+++ web_crawler_doubanTOP250.py
@@ -0,0 +1,15 @@
+import urllib.request
+import re
+
+url = 'https://movie.douban.com/top250'
+req = urllib.request.urlopen(url)
+html = req.read().decode('utf-8')
+
+tit = r'<span class="title">[^"]+</span>'
+titlelist = re.findall(tit,html)
+
+print(len(titlelist))
+
+for ench in titlelist:
+    print(ench)
+#print(html)
