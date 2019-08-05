1d7499abe0bc79e90c3d84c7a54acc5c053c9277
Angel-HW <43689768+Angel-HW@users.noreply.github.com>
2019-08-05 13:18:44 +0800

    Update web_crawler_doubanTOP250.py

--- web_crawler_doubanTOP250.py
+++ web_crawler_doubanTOP250.py
@@ -5,10 +5,8 @@ url = 'https://movie.douban.com/top250'
 req = urllib.request.urlopen(url)
 html = req.read().decode('utf-8')
 
-tit = r'<span class="title">[^"]+</span>'
+tit = r'<img width="100" alt="(.*)" src="([^"]+\.jpg)" class="">+(?:[\s\S]*?)+<span class="rating_num" property="v:average">+(\d\.\d)+</span>+(?:[\s\S]*?)+<span>+([\d]+人评价)+</span>'
 titlelist = re.findall(tit,html)
-
-print(len(titlelist))
 
 for ench in titlelist:
     print(ench)
